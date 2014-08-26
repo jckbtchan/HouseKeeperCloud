27b266b4c9db4c587caede3487d6c53e648e2386
jckbtchan <jckbte@gmail.com>
2014-08-26 16:33:14 +0800

    add time function

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,15 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+	response.success("Hello world!");
+});
+
+AV.Cloud.define("settle_timer", function(request, response) {
+	AV.Cloud.run('hello', {}, {
+		success: function(result) {
+			// result is 'Hello world!'
+		},
+		error: function(error) {}
+	});
+	return res.success();
 });
