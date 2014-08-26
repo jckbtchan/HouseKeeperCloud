9af5569e494ae88bd4db1ff4affee4d14dd736bd
jckbtchan <jckbte@gmail.com>
2014-08-26 19:43:52 +0800

    code cache

--- cloud/main.js
+++ cloud/main.js
@@ -4,6 +4,31 @@ AV.Cloud.define("hello", function(request, response) {
 	response.success("Hello world!");
 });
 
+AV.Cloud.define("query", function(request, response) {
+	var RecordDesc = AV.Object.extend("RecordDesc");
+	var query = new AV.Query(RecordDesc);
+	query.equalTo("hasSettle", "false");
+	query.find({
+		success: function(results) {
+			var length = results.length;
+			var RecordDescs = new Array(length);
+			for (var i = 0; i < length; i++) {
+				var object = results[i];
+				var member = object.get('member');
+				var money = object.get('money');
+				var payer = object.get('payer');
+				RecordDescs[i] = new Array(3);
+				RecordDescs[i][0] = payer;
+				RecordDescs[i][1] = money;
+				RecordDescs[i][2] = member;
+			}
+		},
+		error: function(error) {
+			alert("Error: " + error.code + " " + error.message);
+		}
+	});
+});
+
 AV.Cloud.define("settle_timer", function(request, response) {
 	AV.Cloud.run('hello', {}, {
 		success: function(result) {
@@ -11,5 +36,4 @@ AV.Cloud.define("settle_timer", function(request, response) {
 		},
 		error: function(error) {}
 	});
-	response.success("settle run succeed");
 });
