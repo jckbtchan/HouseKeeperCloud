6b1c9f2fc2b2c33eb073baea5338c95251aefa5f
jckbtchan <jckbte@gmail.com>
2014-08-27 14:10:03 +0800

    add

--- cloud/main.js
+++ cloud/main.js
@@ -4,36 +4,84 @@ AV.Cloud.define("hello", function(request, response) {
 	response.success("Hello world!");
 });
 
-AV.Cloud.define("query", function(request, response) {
+AV.Cloud.define("record_desc", function(request, response) {
 	var RecordDesc = AV.Object.extend("RecordDesc");
-	var query = new AV.Query(RecordDesc);
-	query.equalTo("hasSettle", "false");
-	query.find({
+	var descQuery = new AV.Query(RecordDesc);
+	descQuery.equalTo("hasSettle", "false");
+	descQuery.find({
 		success: function(results) {
 			var length = results.length;
 			var RecordDescs = new Array(length);
 			for (var i = 0; i < length; i++) {
 				var object = results[i];
-				var member = object.get('member');
-				var money = object.get('money');
 				var payer = object.get('payer');
+				var money = object.get('money');
+				var member = object.get('member');
 				RecordDescs[i] = new Array(3);
 				RecordDescs[i][0] = payer;
 				RecordDescs[i][1] = money;
 				RecordDescs[i][2] = member;
 			}
+			response.success(RecordDescs);
 		},
 		error: function(error) {
-			alert("Error: " + error.code + " " + error.message);
+			response.error("Error: " + error.code + " " + error.message);
 		}
 	});
 });
 
-AV.Cloud.define("settle_timer", function(request, response) {
-	AV.Cloud.run('hello', {}, {
-		success: function(result) {
-			// result is 'Hello world!'
+AV.Cloud.define("record_result", function(request, response) {
+	var RecordDescs = request.object.get("RecordDescs");
+	var payer = RecordDescs[0];
+	var money = RecordDescs[1];
+	var member = RecordDescs[2];
+
+	var RecordResult = AV.Object.extend("RecordResult");
+	var resultQuery = new AV.Query(RecordResult);
+	resultQuery.equalTo("payer", payer);
+	resultQuery.find({
+		success: function(results) {
+			var object = results[0];
+			var memberNumber = member.length;
+			var average = money / memberNumber;
+			for (var i = 0; i < memberNumber; i++) {
+				if (member[i] != payer) {
+					var original = object.get(member[i]);
+					object.set(member[i], original + average);
+				}
+			};
+			object.save();
+			response.success();
 		},
-		error: function(error) {}
+		error: function(error) {
+			response.error("Error: " + error.code + " " + error.message);
+		}
+	});
+});
+
+AV.Cloud.define("push", function(request, response) {
+	response.success("Push!");
+});
+
+AV.Cloud.define("settle_timer", function(request, response) {
+	AV.Cloud.run('record_desc', {}, {
+		success: function(result) {
+			var length = results.length;
+			for (var i = 0; i < length; i++) {
+				AV.Cloud.run("record_result", {
+					RecordDescs: results[i]
+				}, {
+					success: function(result) {
+						
+					},
+					error: function(err) {
+						alert("Error: " + error.code + " " + error.message);
+					}
+				});
+			}
+		},
+		error: function(error) {
+			alert("Error: " + error.code + " " + error.message);
+		}
 	});
 });
